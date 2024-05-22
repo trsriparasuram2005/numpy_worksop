c1d2923ae207856ebe60f24b95d9e85089f894df
trsriparasuram2005 <154000181+trsriparasuram2005@users.noreply.github.com>
2024-05-22 10:59:27 +0530

    Update question7.py

--- question7.py
+++ question7.py
@@ -9,4 +9,18 @@ e=[3]
 # use these lists to create another list which will look like the list output in line 2
 
 # use append method to do this.
+utput = []
 
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [7, 5, 3, 2, 3]
+d = [2]
+e = [3]
+output.append(a)
+output.append(b)
+output.append(c)
+c.append(d)
+d.append(e)
+
+
+print(output)
